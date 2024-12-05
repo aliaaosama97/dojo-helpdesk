ebb23439729d6cb3d00343c34f5485544e774a6c
Aliaa osama <aliaaosama97@gmail.com>
2024-12-05 14:36:10 +0200

    nextJs tut 7

---
+++ app/tickets/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TicketList from './TicketList'
+
+export default function Tickets() {
+  return (
+    <main>
+      <nav>
+        <h2>Tickets</h2>
+        <p><small>Currently open tickets.</small></p>
+      </nav>
+
+      <TicketList/>
+    </main>
+  )
+}
